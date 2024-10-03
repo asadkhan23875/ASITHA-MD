24ee3e06bdf724ed9f27490c03ca56ad15c39632
asadkhan23875 <152064270+asadkhan23875@users.noreply.github.com>
2024-10-03 13:50:48 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=qMtzGC6L#SRS61HNgzmA6Rcl_8uURWt9geswTfCcuUKRWhvlYeSw",
+MONGODB: process.env.MONGODB || "mongodb+srv://hanifkamran020:<db_password>@cluster0.bw4yw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",//enter mongo db url
 };
